b66ef6d3c079def613345913d7f406b4007e0d94
rouaud <rouaud@ibpc.fr>
2024-05-17 15:58:10 +0200

    Correct prettierd setup.

--- lua/plugins/nonels.lua
+++ lua/plugins/nonels.lua
@@ -1,56 +1,62 @@
 -- None-LS, fork of Null-LS.
 return {
-    "nvimtools/none-ls.nvim",
-    config = function()
-        local null_ls = require("null-ls")
+    {
+        "nvimtools/none-ls.nvim",
+        config = function()
+            local null_ls = require("null-ls")
 
-        null_ls.setup({
-            sources = {
-                null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.prettierd,
-            },
-        })
+            null_ls.setup({
+                sources = {
+                    null_ls.builtins.formatting.stylua,
+                    null_ls.builtins.formatting.prettierd,
+                },
+            })
 
-        vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {})
+            vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {})
+        end,
+    },
+    {
+        "MunifTanjim/prettier.nvim",
+        config = function()
+            -- Setup prettierd.
+            local prettier = require("prettier")
 
-        -- Setup prettierd.
-        local prettier = require("prettier")
-
-        prettier.setup({
-            bin = "prettierd",
-            filetypes = {
-                "css",
-                "graphql",
-                "html",
-                "javascript",
-                "javascriptreact",
-                "json",
-                "less",
-                "markdown",
-                "scss",
-                "typescript",
-                "typescriptreact",
-                "yaml",
-            },
-            cli_options = {
-                arrow_parens = "always",
-                bracket_spacing = true,
-                bracket_same_line = false,
-                embedded_language_formatting = "auto",
-                end_of_line = "lf",
-                html_whitespace_sensitivity = "css",
-                jsx_single_quote = false,
-                print_width = 80,
-                prose_wrap = "preserve",
-                quote_props = "as-needed",
-                semi = true,
-                single_attribute_per_line = false,
-                single_quote = false,
-                tab_width = 4,
-                trailing_comma = "es5",
-                use_tabs = false,
-                vue_indent_script_and_style = false,
-            },
-        })
-    end,
+            prettier.setup({
+                bin = "prettierd",
+                filetypes = {
+                    "css",
+                    "graphql",
+                    "html",
+                    "javascript",
+                    "javascriptreact",
+                    "json",
+                    "less",
+                    "markdown",
+                    "scss",
+                    "typescript",
+                    "typescriptreact",
+                    "yaml",
+                },
+                cli_options = {
+                    arrow_parens = "always",
+                    bracket_spacing = true,
+                    bracket_same_line = false,
+                    embedded_language_formatting = "auto",
+                    end_of_line = "lf",
+                    html_whitespace_sensitivity = "css",
+                    jsx_single_quote = false,
+                    print_width = 80,
+                    prose_wrap = "preserve",
+                    quote_props = "as-needed",
+                    semi = true,
+                    single_attribute_per_line = false,
+                    single_quote = false,
+                    tab_width = 4,
+                    trailing_comma = "es5",
+                    use_tabs = false,
+                    vue_indent_script_and_style = false,
+                },
+            })
+        end,
+    },
 }
