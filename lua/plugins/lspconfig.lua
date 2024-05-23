3b9c5869d9cba26ba8e03af16a0278dd68bd124d
rouaud <rouaud@ibpc.fr>
2024-05-23 17:11:01 +0200

    JUST WANTING SOME RENAME AND FORMAT. 2MUCH2ASK???

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,65 +1,73 @@
 -- Adding Language Servers (LS) features.
 -- Table of wantes LS.
 local installed_ls = {
-	["lua_ls"] = {},
-	["ltex"] = {},
-	["markdown_oxide"] = {},
-	["r_language_server"] = {},
-	["pyright"] = {},
-	["html"] = {},
-	["cssls"] = {},
-	["rust_analyzer"] = {},
-	["bashls"] = {},
+    ["lua_ls"] = {},
+    ["ltex"] = {},
+    ["markdown_oxide"] = {},
+    ["r_language_server"] = {},
+    ["pyright"] = {},
+    ["ruff"] = {
+        init_options = {
+            settings = {
+                lint = { args = { "--line-length=80" } },
+                format = { args = { "--line-length=80" } },
+            },
+        },
+    },
+    ["html"] = {},
+    ["cssls"] = {},
+    ["rust_analyzer"] = {},
+    ["bashls"] = {},
 }
 
 -- Setting things up.
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			local lsp_list = {}
-			local i = 1
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            local lsp_list = {}
+            local i = 1
 
-			for ls_i, _ in pairs(installed_ls) do
-				lsp_list[i] = ls_i
+            for ls_i, _ in pairs(installed_ls) do
+                lsp_list[i] = ls_i
 
-				i = i + 1
-			end
+                i = i + 1
+            end
 
-			require("mason-lspconfig").setup({
-				ensure_installed = lsp_list,
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			-- Adding snippet based on LSP.
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            require("mason-lspconfig").setup({
+                ensure_installed = lsp_list,
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            -- Adding snippet based on LSP.
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			-- Global LSP configuration.
-			local lspconfig = require("lspconfig")
+            -- Global LSP configuration.
+            local lspconfig = require("lspconfig")
 
-			-- Automatically setup all LS.
-			for ls_i, parameter_i in pairs(installed_ls) do
-				-- LSP snippets.
-				parameter_i["capabilities"] = capabilities
+            -- Automatically setup all LS.
+            for ls_i, parameter_i in pairs(installed_ls) do
+                -- LSP snippets.
+                parameter_i["capabilities"] = capabilities
 
-				-- Other configurations.
-				lspconfig[ls_i].setup(parameter_i)
-			end
+                -- Other configurations.
+                lspconfig[ls_i].setup(parameter_i)
+            end
 
-			-- Setting new keymap.
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "<leader>rf", vim.lsp.buf.rename, {})
-		end,
-	},
+            -- Setting new keymap.
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+            vim.keymap.set("n", "<leader>rf", vim.lsp.buf.rename, {})
+        end,
+    },
 }
