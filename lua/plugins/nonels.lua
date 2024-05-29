ed0fddb4baa09014938cb4b3305a88fc76e1d099
FilouPlains <lucas.rouaud@gmail.com>
2024-05-29 15:07:07 +0200

    Adding LaTeX support.

--- lua/plugins/nonels.lua
+++ lua/plugins/nonels.lua
@@ -1,87 +1,96 @@
 -- None-LS, fork of Null-LS.
 -- List for files for "prettierd".
 local file_type = {
-    "css",
-    "graphql",
-    "html",
-    "javascript",
-    "javascriptreact",
-    "json",
-    "less",
-    "markdown",
-    "scss",
-    "typescript",
-    "typescriptreact",
-    "yaml",
+	"css",
+	"graphql",
+	"html",
+	"javascript",
+	"javascriptreact",
+	"json",
+	"less",
+	"markdown",
+	"scss",
+	"typescript",
+	"typescriptreact",
+	"yaml",
+}
+
+local format_list = {
+	"stylua",
+	"prettierd",
 }
 
 return {
-    {
-        "nvimtools/none-ls.nvim",
-        config = function()
-            local null_ls = require("null-ls")
+	{
+		"nvimtools/none-ls.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = format_list,
+			})
 
-            null_ls.setup({
-                sources = {
-                    null_ls.builtins.formatting.stylua,
-                    null_ls.builtins.formatting.prettierd,
-                },
-            })
+			local null_ls = require("null-ls")
+			local null_format_list = {}
 
-            -- Function that format a file using "Prettier" if good extension
-            -- file detected or another one (bring with LSP or NoneLS).
-            local function format()
-                local view = vim.fn.winsaveview()
+			for i, format_i in pairs(format_list) do
+				null_format_list[i] = null_ls.builtins.formatting[format_i]
+			end
 
-                local launch_format = true
+			null_ls.setup({ sources = null_format_list })
 
-                for _, type in pairs(file_type) do
-                    if type == vim.bo.filetype then
-                        launch_format = false
-                        vim.cmd("Prettier")
-                    end
-                end
+			-- Function that format a file using "Prettier" if good extension
+			-- file detected or another one (bring with LSP or NoneLS).
+			local function format()
+				local view = vim.fn.winsaveview()
 
-                if launch_format then
-                    vim.cmd("lua vim.lsp.buf.format()")
-                end
+				local launch_format = true
 
-                vim.fn.winrestview(view)
-            end
+				for _, type in pairs(file_type) do
+					if type == vim.bo.filetype then
+						launch_format = false
+						vim.cmd("Prettier")
+					end
+				end
 
-            -- Command to format a file.
-            vim.keymap.set("n", "<leader>f", format, {})
-        end,
-    },
-    {
-        "MunifTanjim/prettier.nvim",
-        config = function()
-            -- Setup prettierd.
-            local prettier = require("prettier")
+				if launch_format then
+					vim.cmd("lua vim.lsp.buf.format()")
+				end
 
-            prettier.setup({
-                bin = "prettierd",
-                filetypes = file_type,
-                cli_options = {
-                    arrow_parens = "always",
-                    bracket_spacing = true,
-                    bracket_same_line = false,
-                    embedded_language_formatting = "auto",
-                    end_of_line = "lf",
-                    html_whitespace_sensitivity = "css",
-                    jsx_single_quote = false,
-                    print_width = 80,
-                    prose_wrap = "preserve",
-                    quote_props = "as-needed",
-                    semi = true,
-                    single_attribute_per_line = false,
-                    single_quote = false,
-                    tab_width = 4,
-                    trailing_comma = "es5",
-                    use_tabs = false,
-                    vue_indent_script_and_style = false,
-                },
-            })
-        end,
-    },
+				vim.fn.winrestview(view)
+			end
+
+			-- Command to format a file.
+			vim.keymap.set("n", "<leader>f", format, {})
+		end,
+	},
+	{
+		"MunifTanjim/prettier.nvim",
+		config = function()
+			-- Setup prettierd.
+			local prettier = require("prettier")
+
+			prettier.setup({
+				bin = "prettierd",
+				filetypes = file_type,
+				cli_options = {
+					arrow_parens = "always",
+					bracket_spacing = true,
+					bracket_same_line = false,
+					embedded_language_formatting = "auto",
+					end_of_line = "lf",
+					html_whitespace_sensitivity = "css",
+					jsx_single_quote = false,
+					print_width = 80,
+					prose_wrap = "preserve",
+					quote_props = "as-needed",
+					semi = true,
+					single_attribute_per_line = false,
+					single_quote = false,
+					tab_width = 4,
+					trailing_comma = "es5",
+					use_tabs = false,
+					vue_indent_script_and_style = false,
+				},
+			})
+		end,
+	},
 }
---
+++ lua/plugins/vimtex.lua
@@ -0,0 +1,13 @@
+-- Add LaTeX support.
+return {
+	"lervag/vimtex",
+	lazy = false,
+	init = function()
+		vim.cmd([[
+            filetype plugin indent on
+            let g:vimtex_view_general_viewer = "okular"
+            let maplocalleader = " "
+            let g:vimtex_view_general_options = "--unique file:@pdf\#src:@line@tex"
+        ]])
+	end,
+}
